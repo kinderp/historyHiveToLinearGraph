1b1309f8db09c73385c151e31cd56ed7404ee79f
Gioakk <gioakbombaci@gmail.com>
2015-01-21 19:46:55 +0100

    adattamento dello script alla nuova struttura dati (observations)

--- js/main.js
+++ js/main.js
@@ -4,10 +4,13 @@
 (function () {
 	
 	"use strict";
+	var observed_prop, poiType, poiID;
 
     var changeObs = function changeObs(sensorPOI) {
         var sensorId, from, to, sensorParsed, numberOfDays, today;
+        var entityDataPOI;
 
+        /*
         // get the sensorId
         if (sensorPOI != 'Taxi1') {
             sensorParsed = JSON.parse(sensorPOI);
@@ -20,7 +23,30 @@
         	sensorId = 'Taxi1';
             
         }
-
+		*/
+        sensorParsed = JSON.parse(sensorPOI);
+        entityDataPOI = sensorParsed.poi.data;
+        poiID = sensorParsed.poi.id;
+        
+        //se l'entità cliccata sulla mappa è un observations
+        if(entityDataPOI.type == "observations"){
+        	poiType = "observations";
+        	
+        	//controllo qual'è l'observed_property dell'observations sulla base della presenza dell'attributo Service1/2/3
+        	if(entityDataPOI.Service1)
+        		observed_prop = "pm10";
+        	else if(entityDataPOI.Service2)
+        		observed_prop = "temperature";
+        	else if(entityDataPOI.Service3)
+        		observed_prop = "pressure";
+        }
+        
+        //se l'entità cliccata sulla mappa è un taxi
+        else if(entityDataPOI.type == "Taxi"){
+        	poiType = "Taxi";
+        }
+        	
+        /*
         // Change to the new Sensor
         var requestParams = {
             "method": "GET",
@@ -28,6 +54,14 @@
             "onSuccess": processHistoricData.bind(this, sensorId),
             "onFailure": onFailure.bind(this, sensorId)
         };
+        */
+        var requestParams = {
+                "method": "GET",
+                //"supportsAccessControl": true,
+                "onSuccess": processHistoricData.bind(this),
+                "onFailure": onFailure.bind(this)
+            };
+        
 
         // Send loading msg by OutputStatus endpoint
         var loadMsg = 'loading ' + sensorId;
@@ -51,13 +85,25 @@
             MashupPlatform.http.makeRequest(url + "/regulators/between/" + sensorId + '/' + from + '/' + to, requestParams);
         }
         */
+        
+        /*
         if (sensorId.match('Taxi1')){
         	 MashupPlatform.http.makeRequest(url + "/taxi/" + sensorId, requestParams);
+        }
+        */
+        
+        if(poiType.match('Taxi')){
+        	loadMsg = "It's not possible to graph data of a taxi!\nClick on an observation on the map";
+            MashupPlatform.wiring.pushEvent("OutputStatus", JSON.stringify(loadMsg));
+        	
+        }
+        else if(poiType.match('observations')){
+        	MashupPlatform.http.makeRequest(url + "/observations?id=" + poiID +"&type=observations", requestParams);
         }
     };
     
     
-    var onFailure = function onFailure(sensorId) {
+    var onFailure = function onFailure(/*sensorId*/) {
         // Use demo data if the request fails
 
         var parsedResponse = {
@@ -69,7 +115,7 @@
     };
 
     
-    var processHistoricData = function processHistoricData(sensorId, response) {
+    var processHistoricData = function processHistoricData(/*sensorId*/response) {
         var i, parsedResponse, data, id, lampObservationId, config;
 
         // First element is the older.
@@ -80,6 +126,7 @@
             var parsedResponseText = response.responseText;
             parsedResponse = JSON.parse(response.responseText);
 
+            /*
             var data2 = parsedResponse.date;
             var temp2 = parsedResponse.temperature;
             var pres2 = parsedResponse.pressure;
@@ -90,46 +137,89 @@
             var temp3 = elem2.temperature;
             var pres3 = elem2.pressure;
             var pm103 = elem2.pm10;
+            */
             
             
         //}
 
         id = 69;
 
-        if (sensorId.match('Taxi1')) {
+        //if (sensorId.match('Taxi1')) {
+        if (poiType.match('observations')) {
             // Axis config
-            config = {
+        	if(observed_prop == "temperature"){
+        	config = {
                 'axisConfig': [
                     {
                         axisId: 0,
-                        label: 'Temperature',
+                        label: observed_prop,
                         color: '#00A8F0',
                         max: 300,
                         min: -300,
                         ticks: [-300, -150, -100, -50, 0, 50, 100, 150, 200, 250, 300]
-                    },
-                    {
-                        axisId: 1,
-                        label: 'Pressure',
-                        color: '#93a600',
-                        max: 300,
-                        min: 0,
-                        ticks: [0, 50, 100, 150, 200, 250, 300]
-                    },
-                    {
-                        axisId: 2,
-                        label: 'PM10',
-                        color: '#CB4B4B',
-                        max: 100000,
-                        min: 0,
-                        ticks: [0, 10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000]
                     }
                 ],
-                'title': sensorId,
+                'title': poiID,
                 'leyend': {
                     position: 'ne'
                 }
             };
+        }
+        	else if(observed_prop == "pressure"){
+                config = {
+                    'axisConfig': [
+                        {
+                            axisId: 0,
+                            label: observed_prop,
+                            color: '#93a600',
+                            max: 300,
+                            min: 0,
+                            ticks: [0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300]
+                        }
+                    ],
+                    'title': poiID,
+                    'leyend': {
+                        position: 'ne'
+                    }
+                };
+        	}
+        	else if(observed_prop == "pm10"){
+                config = {
+                    'axisConfig': [
+                        {
+                            axisId: 0,
+                            label: observed_prop,
+                            color: '#CB4B4B',
+                            max: 100000,
+                            min: 0,
+                            ticks: [0, 10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000]
+                        }
+                    ],
+                    'title': poiID,
+                    'leyend': {
+                        position: 'ne'
+                    }
+                };
+        	}
+        	
+        	/*
+            {
+                axisId: 1,
+                label: 'Pressure',
+                color: '#93a600',
+                max: 300,
+                min: 0,
+                ticks: [0, 50, 100, 150, 200, 250, 300]
+            },
+            {
+                axisId: 2,
+                label: 'PM10',
+                color: '#CB4B4B',
+                max: 100000,
+                min: 0,
+                ticks: [0, 10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000]
+            }
+            */
             MashupPlatform.wiring.pushEvent("OutputStatus", JSON.stringify(config));
 
             data = [];
@@ -137,6 +227,7 @@
                 // History-Mod internal id
                 //lampObservationId = parsedResponse[i].id;
             	var elem = JSON.parse(parsedResponse[i]);
+            	/*
                 data.push({
                     'id': 0,
                     'value': [elem.date, elem.temperature],
@@ -154,6 +245,13 @@
                     'value': [elem.date, elem.pm10],
                     'label': 'PM10',
                     'axis': 3
+                });
+                */
+                data.push({
+                    'id': 0,
+                    'value': [elem.date, elem.value],
+                    'label': observed_prop,
+                    'axis': 1
                 });
             }
         // AMMS
